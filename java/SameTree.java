2a9a96a0c8d7bea42aa38e7b0a0daed323d9174d
Esteban Puyana <epuyana@sqg.co>
2024-11-11 18:20:04 -0500

    updated nov 11 6:20 pm EST

--- java/SameTree.java
+++ java/SameTree.java
@@ -1,24 +1,18 @@
-import java.util.ArrayList;
-import java.util.Stack;
-
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
 
+        //If both root nodes are null, then the tree is the same.
         if(p == null && q == null){
-            return true; 
-        }
-        
-        return true; //PLACEHOLDER WHILE SOLVING ALGO
+            return true;
+        // If either p or q is null but the other is not, return false, as the tree is not the same.    
+        } else if ((p != null && q == null) || (p == null && q != null)) {
+            return false;
+        // if the values of the trees differ at any point, they are not the same, return false.    
+        } else if(p.val != q.val) {
+            return false;
+        } 
 
-
-        while(p != null && q != null){
-            if (p.val == q.val) {
-                
-
-            }
-
-           
+        // run the function recursively on the left and right childs of the tree. 
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
-
-}}
-
+}
--- java/SubTreeOfTree.java
+++ java/SubTreeOfTree.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
 
-        return isIdentical(root, subRoot) || 
-        isIdentical(root.left, subRoot) ||
-        isIdentical(root.right, subRoot);
+        if(root == null){
+            return subRoot == null;
+        }
 
+        return isIdentical(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
     }
 
     public boolean isIdentical(TreeNode node, TreeNode subNode){
@@ -12,7 +13,7 @@ class Solution {
         if(node == null && subNode == null){
             return true;
         // If either p or q is null but the other is not, return false, as the tree is not the same.    
-        } else if ((node != null && subNode == null) || (node == null && subNode != null)) {
+        } else if ((node == null || subNode == null)) {
             return false;
         // if the values of the trees differ at any point, they are not the same, return false.    
         } else if(node.val != subNode.val) {
